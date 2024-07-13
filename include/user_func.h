68851eafca8de03b00711c9833976640d2d4c692
ldellape <luca.dellapenna@studenti.unipg.it>
2024-07-13 18:01:18 +0200

    output included

--- include/user_func.h
+++ include/user_func.h
@@ -157,7 +157,7 @@ void printlogo(){
 
 std::cout << green << textCMS  << reset << std::endl;
 std::cout<< green << "Test su cavo PS-PP1" << reset <<std::endl;
-std::system("mkdir -p ./output/plots && mkdir -p ./output/plots/SingleCable && mkdir -p ./output/plots/CheckCable && mkdir -p ./output/report" );
+std::system("mkdir -p ./output/plots && mkdir -p ./output/plots/SingleCable && mkdir -p ./output/plots/CheckCable && mkdir -p ./output/report && mkdir -p ./output/plotsTimeResistence" );
 
 }
 
