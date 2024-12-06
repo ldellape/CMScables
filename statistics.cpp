e03f64544740de0ad4e67598a393f6a2e98461b5
ldellape <luca.dellapenna@pg.infn.it>
2024-12-06 10:15:20 +0100

    new column added

--- statistics.cpp
+++ statistics.cpp
@@ -25,28 +25,9 @@
 
 TFile *f_StatOut;
 
-void UpdateHisto(ROOT::RDataFrame df, std::string_view column, std::string sTitle, std::string_view cut, Bool_t update, Int_t Nbins){
-    if(!update){
-     ROOT::RDF::RResultPtr<::TH1D> *h;
-     h = new ROOT::RDF::RResultPtr<::TH1D>(df.Filter(cut).Histo1D({sTitle.c_str(), sTitle.c_str(), Nbins, df.Filter(cut).Min(column).GetValue(), df.Filter(cut).Max(column).GetValue()}, column));
-     TH1D *hh = (TH1D*) h->GetPtr();
-     if(hh->GetEntries()!=0) hh->Write();
-    }
-    else if(update){
-      TH1D *h = (TH1D*) f_StatOut->Get(sTitle.c_str());  
-     if(h){ 
-      double minX = std::min(h->GetXaxis()->GetXmin(), df.Filter(cut).Min(column).GetValue());
-      double maxX = std::max(h->GetXaxis()->GetXmax(), df.Filter(cut).Min(column).GetValue());
-      auto h_temp = df.Filter(cut).Histo1D({sTitle.c_str(), sTitle.c_str(), Nbins, h->GetXaxis()->GetXmin(), h->GetXaxis()->GetXmax()}, column);
-      h->Add(h_temp.GetPtr());  
-      if(h->GetEntries()!=0) h->Write("", TObject::kOverwrite);
-     } 
-    }
-     else{
-      std::cerr << "Histogram " << sTitle << " not found!" << std::endl;
-    }
-}
-
+// ******************************************************************** //
+// * Function to read the test and build trees                        * //
+// ******************************************************************** //
 void ReadOutput(const std::string TestNameFile, Int_t file, TString option) {
     TFile *f_output = new TFile(("stat_root/" + std::to_string(file) + ".root").c_str(), "RECREATE");
     TTree *TestResultContinuity = new TTree("TestResultContinuity", "TestResultContinuity");
@@ -57,7 +38,10 @@ void ReadOutput(const std::string TestNameFile, Int_t file, TString option) {
     Bool_t statusCon, statusIns, channelPH_Ins, channelTsensor_Ins;
     Bool_t pspp1_test_Con, fullchain_test_Con;
     Bool_t pspp1_test_Ins, fullchain_test_Ins;
+    std::string channelName_Con;
+    std::string channelName_Ins;
 
+    TestResultContinuity->Branch("channelName_Con", &channelName_Con);
     TestResultContinuity->Branch("resistenceCon", &resistenceCon, "resistenceCon/F");
     TestResultContinuity->Branch("channelLV_Con", &channelLV_Con, "channelLV_Con/O");
     TestResultContinuity->Branch("channelHV_Con", &channelHV_Con, "channelHV_Con/O");
@@ -67,6 +51,7 @@ void ReadOutput(const std::string TestNameFile, Int_t file, TString option) {
     TestResultContinuity->Branch("pspp1_test_Con", &pspp1_test_Con, "pspp1_test_Con/O");
     TestResultContinuity->Branch("fullchain_test_Con", &fullchain_test_Con, "fullchain_test_Con/O");
 
+    TestResultIsolation->Branch("channelName_Ins", &channelName_Ins);
     TestResultIsolation->Branch("resistenceIns", &resistenceIns, "resistenceIns/F");
     TestResultIsolation->Branch("channelLV_Ins", &channelLV_Ins, "channelLV_Ins/O");
     TestResultIsolation->Branch("channelHV_Ins", &channelHV_Ins, "channelHV_Ins/O");
@@ -114,6 +99,7 @@ void ReadOutput(const std::string TestNameFile, Int_t file, TString option) {
         channelTsensor_Con = false;
         pspp1_test_Con = false;
         fullchain_test_Con = false;
+        channelName_Con = (std::get<1>(it)).c_str();
         if( std::get<1>(it).find("LV") != std::string::npos) channelLV_Con = true;
         else if (std::get<1>(it).find("HV") != std::string::npos) channelHV_Con = true; 
         else if( std::get<1>(it).find("PH") != std::string::npos) channelPH_Con = true;
@@ -131,6 +117,7 @@ void ReadOutput(const std::string TestNameFile, Int_t file, TString option) {
         channelTsensor_Ins = false;
         pspp1_test_Ins = false;
         fullchain_test_Ins = false;
+        channelName_Ins = (std::get<1>(it)).c_str();
         if( std::get<1>(it).find("LV") != std::string::npos) channelLV_Ins = true;
         else if (std::get<1>(it).find("HV") != std::string::npos) channelHV_Ins = true; 
         else if( std::get<1>(it).find("PH") != std::string::npos) channelPH_Ins = true;
@@ -143,6 +130,33 @@ void ReadOutput(const std::string TestNameFile, Int_t file, TString option) {
     TestResultContinuity->Write();
     TestResultIsolation->Write();
     f_output->Close(); 
+}
+// ******************************************************************** //
+// ******************************************************************** //
+
+// ******************************************************************** //
+// * funtion to update or create histograms                           * //
+// ******************************************************************** //
+void UpdateHisto(ROOT::RDataFrame df, std::string_view column, std::string sTitle, std::string_view cut, Bool_t update, Int_t Nbins){
+    if(!update){
+     ROOT::RDF::RResultPtr<::TH1D> *h;
+     h = new ROOT::RDF::RResultPtr<::TH1D>(df.Filter(cut).Histo1D({sTitle.c_str(), sTitle.c_str(), Nbins, df.Filter(cut).Min(column).GetValue(), df.Filter(cut).Max(column).GetValue()}, column));
+     TH1D *hh = (TH1D*) h->GetPtr();
+     if(hh->GetEntries()!=0) hh->Write();
+    }
+    else if(update){
+      TH1D *h = (TH1D*) f_StatOut->Get(sTitle.c_str());  
+     if(h){ 
+      double minX = std::min(h->GetXaxis()->GetXmin(), df.Filter(cut).Min(column).GetValue());
+      double maxX = std::max(h->GetXaxis()->GetXmax(), df.Filter(cut).Min(column).GetValue());
+      auto h_temp = df.Filter(cut).Histo1D({sTitle.c_str(), sTitle.c_str(), Nbins, h->GetXaxis()->GetXmin(), h->GetXaxis()->GetXmax()}, column);
+      h->Add(h_temp.GetPtr());  
+      if(h->GetEntries()!=0) h->Write("", TObject::kOverwrite);
+     } 
+    }
+     else{
+      std::cerr << "Histogram " << sTitle << " not found!" << std::endl;
+    }
 }
 // ******************************************************************** //
 
@@ -166,7 +180,6 @@ int main(int argc, char* argv[]){
  std::system("mkdir  stat_root");
  std::system("mkdir -p stat");
  std::set<std::string> tests;
-
  std::vector<std::string> FileNames;
  std::vector<std::string> FileNamesProcessed;
  std::vector<std::string> sInputDir;
@@ -222,10 +235,10 @@ else{
     std::cout << "creating statistics.root ..." << std::endl;
     f_StatOut = new TFile(pathOutFile, "RECREATE");
 }
-// ----------------------------------------------- //
+// --------------------------------------------------- //
 
 
-// ----------- find new tests to be added on cumulative stat/ rdf -------------- //
+// ----------- find new tests to be added on cumulative stat/rdf -------------- //
 #ifndef DB
         for(int ii=0; ii < int(cable.size()); ii++){
         FileNames.clear();
@@ -308,13 +321,15 @@ else{
 
 
 
+//--------- output root files ------------------------- //
 TChain inputChain_continuity("TestResultContinuity");
 TChain inputChain_isolation("TestResultIsolation");
-inputChain_continuity.Add("./stat_root/*.root");
-inputChain_isolation.Add("./stat_root/*.root");
 ROOT::RDataFrame df_Continuity(inputChain_continuity);
 ROOT::RDataFrame df_Isolation(inputChain_isolation);
 
+inputChain_continuity.Add("./stat_root/*.root");
+inputChain_isolation.Add("./stat_root/*.root");
+//----------------------------------------------------- //
 
 
 // ********************************************************************* //
@@ -326,10 +341,11 @@ df_Continuity.Foreach([&FULLCHAIN_statistics](const Bool_t fc){ if(fc == true){ 
 df_Continuity.Foreach([&PS_PP1_statistics](const Bool_t ps){if(ps == true){ PS_PP1_statistics = true; return;}}, {"pspp1_test_Con"});
 if(PS_PP1_statistics ) CableName.push_back("pspp1");
 if(FULLCHAIN_statistics) CableName.push_back("fullchain");
+
 std::cout<<"Full-Chain: "<<FULLCHAIN_statistics<<std::endl;
 std::cout<<"PS-PP1: "<<PS_PP1_statistics<<std::endl;
-f_StatOut->cd();
 
+f_StatOut->cd();
 for(int i = 0; i < int(CableName.size()); i++){
     UpdateHisto(df_Continuity, "statusCon", Form("h_PassedFailedContinuity_%s", CableName[i].Data()), Form("%s_test_Con == true", CableName[i].Data()), UpdateStat, 2);
     UpdateHisto(df_Isolation, "statusIns", Form("h_PassedFailedIsolation_%s", CableName[i].Data()), Form("%s_test_Ins == true", CableName[i].Data()), UpdateStat, 2);
@@ -352,16 +368,22 @@ for(int i = 0; i < int(CableName.size()); i++){
     UpdateHisto(df_Isolation, "resistenceIns", Form("h_HV_Isolation_Humidity_%s", CableName[i].Data()), Form("(channelHV_Ins == true || channelTsensor_Ins == true) && %s_test_Ins == true", CableName[i].Data()), UpdateStat, 50);
     #endif
 }
+// ********************************************************************* //
+// * END OF HISTOGRAMS                                                 * //
+// ********************************************************************* //
 
-
+double len = 60.1;
+auto df_Continuity2 = df_Continuity.Define("Resistence_per_meter", [len](Float_t res) -> Float_t {  return res/len;}, {"resistenceCon"});
 std::string continuityRoot = "./stat/continuity_rdf.root";
 std::string isolationRoot = "./stat/isolation_rdf.root";
+
+
+
+
+//-------------- saving the files ---------------------- //
 df_Isolation.Snapshot("df_Isolation", isolationRoot);
-df_Continuity.Snapshot("df_Continuity", continuityRoot);
+df_Continuity2.Snapshot("df_Continuity", continuityRoot);
 f_StatOut->Close(); 
-
-
-
 std::cout<<"\033[32mdone \033[0m"<<std::endl;
 std::system("rm -rf stat_root");
 
